1f01c21def9043ef53b37d7f67ac16091a0e7015
Tasos Ventouris <tasosventouris@gmail.com>
2021-05-10 23:07:55 +0300

    1.0.8 version

--- stackprime.php
+++ stackprime.php
@@ -16,7 +16,7 @@
  * Plugin Name:       Stackprime Essentials
  * Plugin URI:        https://www.stackprime.com
  * Description:       A custom plugin from stackprime to unbloat your site from unwanted output. Remove admin nags and notifications, unnecessary items and performance-draining code. Includes security patches and shortcodes.
- * Version:           1.0.7
+ * Version:           1.0.8
  * Author:            stackprime
  * Author URI:        https://www.stackprime.com
  * License:           GPL-2.0+
@@ -35,7 +35,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'STACKPRIME_VERSION', '1.0.7' );
+define( 'STACKPRIME_VERSION', '1.0.8' );
 
 /**
  * The code that runs during plugin activation.
